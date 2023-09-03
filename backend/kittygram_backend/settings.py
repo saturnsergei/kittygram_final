2e1fbe1d2f5356618feb2cedb9c0cc3e98699f03
Сергей Савчишкин <rings.of.saturn@yandex.ru>
2023-09-03 12:28:59 +0200

    environ

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,9 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = os.getenv('DEBUG', False)
 
